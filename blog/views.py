062250859f6aa907e8ee79a3df1211053bfbebeb
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-08 23:10:46 +0900

    Fixed header nav link

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,6 @@ class BaseListView(ListView):
 
     def get_queryset(self):
         queryset = BlogPost.objects.filter(is_public=True).order_by('-created_date').select_related('category')
-        print(queryset.values('category__parent__slug', 'category__slug'))
         return queryset
 
 
@@ -22,24 +21,18 @@ class PostList(BaseListView):
         queryset = super().get_queryset()
         return queryset
 
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     print('これcontext：{}'.format(context['object_list']))
-    #     return context
-
 
 class CategoryList(BaseListView):
     def get_queryset(self):
         category_name = self.kwargs['category']
-        category = SubCategory.objects.get(name=category_name)
-        queryset = super().get_queryset().filter(category=category)
+        category = ParentCategory.objects.get(slug=category_name).slug
+        queryset = super().get_queryset().filter(category__parent__slug=category)
         return queryset
 
 
 class SubCategoryList(BaseListView):
     def get_queryset(self):
         category_name = self.kwargs['category__slug']
-        print('kwargsのデータ{}'.format(category_name))
         category = SubCategory.objects.get(slug=category_name)
         queryset = super().get_queryset().filter(category=category)
         return queryset
