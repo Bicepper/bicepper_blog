2950efdabae8555c98282e628299342d6c34234e
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-12 23:04:02 +0900

    Fixed time_zone, UTC problem.

--- bicepper_blog/settings.py
+++ bicepper_blog/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Tokyo'
 
@@ -143,7 +143,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # dashboard
 GRAPPELLI_INDEX_DASHBOARD = 'dashboard.CustomIndexDashboard'
