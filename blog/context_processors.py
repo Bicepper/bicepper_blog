b453b6523b18e4efe965fddf0e3cfe9b8de9daf2
Shimpei Okamoto <s_okamoto@fancs.com>
2019-06-19 17:41:24 +0900

    Change hard coding to variable at nav.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from .models import (
     ParentCategory,
     SubCategory,
@@ -6,8 +7,10 @@ from .models import (
 
 
 def common(request):
-    category = ParentCategory.objects.all()
-    subcategory = SubCategory.objects.all().values('parent__slug', 'slug')
+    category = ParentCategory.objects.all().prefetch_related('subcategory_set')
+    # for ct in category:
+    #     print(ct.subcategory_set.all())
+    subcategory = SubCategory.objects.all().values('parent__title', 'parent__slug', 'slug')
     ranking = BlogPost.objects.all().order_by('hit_count_generic')[:3]
     archive = BlogPost.objects.order_by('created_date')
     context = {
