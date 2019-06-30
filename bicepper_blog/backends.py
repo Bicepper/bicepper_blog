0037f609a7b792b614a099488e1971ec10e8428a
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-30 19:09:49 +0900

    S3

---
+++ bicepper_blog/backends.py
@@ -0,0 +1,10 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from filebrowser.storage import StorageMixin
+
+
+class PublicMediaStorage(StorageMixin, S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+    pass
+
+
