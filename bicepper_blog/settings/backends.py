d447cde363ff54d110ae16b5b8b282779cc39e84
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-30 19:01:55 +0900

    S3

--- bicepper_blog/settings/backends.py
+++ bicepper_blog/settings/backends.py
@@ -1,9 +1,4 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from filebrowser_safe.storage import S3BotoStorageMixin
-
-
-class S3Storage(S3BotoStorageMixin, S3Boto3Storage):
-    pass
 
 
 class PublicMediaStorage(S3Boto3Storage):
--- bicepper_blog/settings/production.py
+++ bicepper_blog/settings/production.py
@@ -42,6 +42,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, PUBLIC_MEDIA_LOCATION)
 DEFAULT_FILE_STORAGE = 'backends.PublicMediaStorage'
+bicepper_blog.settings.backends.PublicMediaStorage
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
