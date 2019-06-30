cab84a3816905e7123f0cdd4774e5a9b5fa84cf8
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-30 18:40:46 +0900

    S3

--- bicepper_blog/settings/backends.py
+++ bicepper_blog/settings/backends.py
@@ -1,4 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from filebrowser_safe.storage import S3BotoStorageMixin
+
+
+class S3Storage(S3BotoStorageMixin, S3Boto3Storage):
+    pass
 
 
 class MediaStorage(S3Boto3Storage):
