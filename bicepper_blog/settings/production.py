37997ff69c1e230b9376ea15117c6be8ad5fcb18
Shimpei Okamoto <muscleconsole@gmail.com>
2019-06-30 18:15:09 +0900

    S3

--- bicepper_blog/settings/production.py
+++ bicepper_blog/settings/production.py
@@ -40,7 +40,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FILE_STORAGE = 'bicepper_blog.backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # IP制限で許可する
 ALLOWED_IP_BLOCKS = ['127.0.0.1', ]
