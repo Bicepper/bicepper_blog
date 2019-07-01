0cea3800ef0e2edfc9353efff97ce9bf23607f52
Shimpei Okamoto <muscleconsole@gmail.com>
2019-07-01 22:25:26 +0900

    Change staticfiles_dirs

--- bicepper_blog/settings/production.py
+++ bicepper_blog/settings/production.py
@@ -27,7 +27,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = ('/var/www/bicepper_blog_static',)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets")
+]
 
 # IP制限で許可する
 ALLOWED_IP_BLOCKS = ['127.0.0.1', ]
