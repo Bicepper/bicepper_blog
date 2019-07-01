d68945266617dad5b8178f00acdd2af66756730c
Shimpei Okamoto <muscleconsole@gmail.com>
2019-07-02 00:09:04 +0900

    Fixed admin mode.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,4 +1,5 @@
 from django.db.models import Prefetch
+from django.utils import timezone
 from .models import (
     ParentCategory,
     SubCategory,
@@ -9,10 +10,14 @@ from .models import (
 
 
 def common(request):
-    category = ParentCategory.objects.all().prefetch_related('subcategory_set')
-    subcategory = BlogPost.objects.select_related('category').select_related('category__parent').all().order_by('category')
-    ranking = BlogPost.objects.all().order_by('hit_count_generic')[:3]
-    archive = BlogPost.objects.order_by('created_date')
+    category = ParentCategory.objects.all().filter().prefetch_related('subcategory_set')
+    subcategory = BlogPost.objects.filter(is_public=True, is_author=False,
+                                          created_date__lt=timezone.localtime()).select_related(
+        'category').select_related('category__parent').all().order_by('category')
+    ranking = BlogPost.objects.all().filter(is_public=True, is_author=False,
+                                            created_date__lt=timezone.localtime()).order_by('hit_count_generic')[:3]
+    archive = BlogPost.objects.filter(is_public=True, is_author=False,
+                                      created_date__lt=timezone.localtime()).order_by('created_date')
     gatag = GoogleAnalytics.objects.all().values('content')[0]['content'] if GoogleAnalytics.objects.all() else ''
     context = {
         'categories': category,
