ca0f260089dbe5977d9dfad5aa64d7fef87f18ba
Shimpei Okamoto <s_okamoto@fancs.com>
2019-07-04 12:36:35 +0900

    Fixed ranking reverse

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -14,10 +14,9 @@ def common(request):
     subcategory = BlogPost.objects.filter(is_public=True, is_author=False,
                                           created_date__lt=timezone.localtime()).select_related(
         'category').select_related('category__parent').all().order_by('category')
-    ranking = PopularPost.objects.select_related('post_id').all().order_by('view_cnt')
-    print(ranking)
+    ranking = PopularPost.objects.select_related('post_id').all().order_by('view_cnt').reverse()
     archive = BlogPost.objects.filter(is_public=True, is_author=False,
-                                      created_date__lt=timezone.localtime()).order_by('created_date').reverse()
+                                      created_date__lt=timezone.localtime()).order_by('created_date')
     gatag = GoogleAnalytics.objects.all().values('content')[0]['content'] if GoogleAnalytics.objects.all() else ''
     context = {
         'categories': category,
